5d52b6ecfd9172c7d3c1c6afbfb062fd4d8f40ec
Soma Seknar <hi.somasekhar@gmail.com>
2024-01-21 19:35:02 +0100

    html reports name updated

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -30,7 +30,7 @@ public class TestRunner {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
         List<String> jsonPaths = new ArrayList(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "Pet Store Test Execution Report");
+        Configuration config = new Configuration(new File("target"), "Pet Store TestExecution Report");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
